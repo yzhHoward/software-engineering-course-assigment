3bdca90a9748e0c571f4568063b7e274fe8758c9
yzhHoward <yzhhoward@gmail.com>
2019-05-03 19:53:42 +0800

    Add files via upload

---
+++ BlockChainService/src/service/api/BlockChainService.java
@@ -0,0 +1,46 @@
+package service.api;
+
+public interface BlockChainService {
+    /*
+    String QueryTransaction(String recordId);
+    String QueryBalanceChange(String recordId);
+    boolean InsertTransaction(
+            String recordId,
+            int paymentInstitutionId,
+            int paymentUserId,
+            int collectionInstitutionId,
+            int collectionUserId,
+            String dateTime,
+            boolean transactionType,
+            double sum
+    );
+    boolean InsertBalanceChange(
+            String recordId,
+            int institutionId,
+            int userId,
+            String dateTime,
+            boolean recordType,
+            double sum
+    );
+    */
+
+    void setUserName(String userId, String encrypted_message) throws WriteFailureException;
+
+    String getUserName(String userId) throws ReadFailureException;
+
+    void setFinancingApply(String userId, String encrypted_message) throws WriteFailureException;
+
+    String getFinancingApply(String userId) throws ReadFailureException;
+
+    void setContract(String userId, String encrypted_message) throws WriteFailureException;
+
+    String getContract(String userId) throws ReadFailureException;
+
+    void setLoan(String userId, String encrypted_message) throws WriteFailureException;
+
+    String getLoan(String userId) throws ReadFailureException;
+
+    void setRepayment(String userId, String encrypted_message) throws WriteFailureException;
+
+    String getRepayment(String userId) throws ReadFailureException;
+}
--- interface design.java
+++ interface design.java
@@ -1,7 +1,24 @@
-public class Main{
+public class Main {
 
-	public String save_in_chain(String module_source, String encrypted_message) throws WriteFailureException {return "";}
-    /* 
+    public String save_in_chain(String module_source, String encrypted_message) throws WriteFailureException {
+        return "";
+    }
+
+    public void setNameString (String userId, String encrypted_message) throws WriteFailureException {
+    }
+
+    public void getNameString (String userId) throws ReadFailureException {
+        return "";
+    }
+
+    public String save_in_chain(String module_source, String encrypted_message) throws WriteFailureException {
+        return "";
+    }
+
+    public String save_in_chain(String module_source, String encrypted_message) throws WriteFailureException {
+        return "";
+    }
+    /*
      * 此API用于将数据存储入区块链中，所有需要存储信息的模块共同调用这一API存储数据
      * @description: write模块，上游调用此API
      * @param module_source: String，信息来源的模块，
@@ -10,8 +27,10 @@ public class Main{
      * @exception WriteFailureException: 写入失败的时候会抛出我们自定义的WriteFailureException异常
      */
 
-	String fetch_from_chain(String label) throws ReadFailureException {return "";}
-	/* 
+    String fetch_from_chain(String label) throws ReadFailureException {
+        return "";
+    }
+    /*
      * 此API用于从区块链中提取数据，所有需要提取信息的模块共同调用这一API提取数据
      * @description: read模块
      * @param label: String, label的初始来源是save_in_chain API，由上游模块持有
