a8a73142a4fde5f1095b454b4d8b2169567b4e45
yiranyyu <2606375857@qq.com>
2019-05-21 18:33:56 +0800

    Implement chain service

--- chaincode/chainService.go
+++ chaincode/chainService.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
@@ -16,59 +17,112 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 	return shim.Success(nil)
 }
 
-func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	function, args := stub.GetFunctionAndParameters()
-	fmt.Println("invoke is running " + function)
-
-	if function == "test1" {
-		return t.test1(stub, args)
-	} else {
-		return shim.Error("Received unknown function invocation")
-	}
-}
-
-func (t *SimpleChaincode) test1(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	return shim.Success([]byte("Called test1"))
-}
-
 // End User defined chain code struct
 
 // User defined smart contract
 type SmartContract struct{}
 
-func (t *SmartContract) insert_record(stub shim.ChaincodeStubInterface, args []string) string {
-	// TODO: Implement insert_record function
-	return "key of inserted record"
+func (s *SmartContract) Init(APIstub shim.ChaincodeStubInterface) pb.Response {
+	return shim.Success(nil)
 }
 
-func (t *SmartContract) update_record(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	// TODO: Implement update_record
-	return shim.Success([]byte("Call update"))
+func (s *SmartContract) initLedger(APIstub shim.ChaincodeStubInterface) pb.Response {
+	return shim.Success(nil)
 }
 
-func (t *SmartContract) delete_record(stud shim.ChaincodeStubInterface, args []string) pb.Response {
-	// TODO: Implement delete_record
-	return shim.Success([]byte("Call delete"))
+func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) pb.Response {
+	function, args := APIstub.GetFunctionAndParameters()
+
+	if function == "insert_user_info" {
+		return s.insert_user_info(APIstub, args)
+	} else if function == "query_user_info" {
+		return s.query_user_info(APIstub, args)
+	} else if function == "query_contract_record" {
+		return s.query_record(APIstub, args)
+	} else if function == "query_financing_apply_record" {
+		return s.query_record(APIstub, args)
+	} else if function == "query_loan_record" {
+		return s.query_record(APIstub, args)
+	} else if function == "query_repayment_record" {
+		return s.query_record(APIstub, args)
+	} else if function == "insert_contract_record" {
+		return s.insert_record(APIstub, args)
+	} else if function == "insert_financing_apply_record" {
+		return s.insert_record(APIstub, args)
+	} else if function == "insert_loan_record" {
+		return s.insert_record(APIstub, args)
+	} else if function == "insert_repayment_record" {
+		return s.insert_record(APIstub, args)
+	} else if function == "initedger" {
+		return s.initLedger(APIstub)
+	}
+	return shim.Error("Invalid Smart Contract function name")
+}
+
+func (t *SmartContract) insert_user_info(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// args[0]: username
+	// args[1]: message
+	if len(args) != 2 {
+		return shim.Error("Incorrect number of arguments. Expecting 2")
+	}
+	var user_info = UserInfo{
+		EncrypedMessage: args[1]}
+	fmt.Println(user_info)
+	user_info_as_bytes, other_info := json.Marshal(user_info)
+	fmt.Println(other_info)
+	fmt.Println(user_info_as_bytes)
+	err := stub.PutState(args[0], user_info_as_bytes)
+	fmt.Println(err)
+	return shim.Success(nil)
+}
+
+func (t *SmartContract) query_user_info(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Excepting 1")
+	}
+	user_info_as_bytes, _ := stub.GetState(args[0])
+	return shim.Success(user_info_as_bytes)
+}
+
+func (t *SmartContract) insert_record(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// args[0]: record id
+	// args[1]: message
+	if len(args) != 2 {
+		return shim.Error("Incorrect number of arguments. Expecting 2")
+	}
+	var record = Record{
+		EncrypedMessage: args[1]}
+	fmt.Println(record)
+	user_info_as_bytes, other_info := json.Marshal(record)
+	fmt.Println(other_info)
+	fmt.Println(user_info_as_bytes)
+	err := stub.PutState(args[0], user_info_as_bytes)
+	fmt.Println(err)
+	return shim.Success(nil)
+}
+
+func (t *SmartContract) query_record(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Excepting 1")
+	}
+	record_as_bytes, _ := stub.GetState(args[0])
+	return shim.Success(record_as_bytes)
 }
 
 // End user defined smart contrct
 
 // Data type
-type Transaction struct {
-	DateTime string `json:"date_time"`
-	Type     string `json:"sum"`
+type Record struct {
+	EncrypedMessage string `json:"encryped_message"`
 }
-
-func (t *Transaction) generate_key() string {
-	// TODO: implement key generator function, return value must be unique for every different instance
-	// and be constant to same Transaction object
-	return "dumb_key"
+type UserInfo struct {
+	EncrypedMessage string `json:"encryped_message"`
 }
 
 // End date type
 
 func main() {
-	err := shim.Start(new(SimpleChaincode))
+	err := shim.Start(new(SmartContract))
 	if err != nil {
 		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}
