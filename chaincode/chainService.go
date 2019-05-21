4b19a6e18e09e9e04e965809d327394beca2c2c4
yiranyyu <2606375857@qq.com>
2019-05-21 15:29:28 +0800

    Define smartcontract interfaces

--- chaincode/chainService.go
+++ chaincode/chainService.go
@@ -3,33 +3,73 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
-   "fmt"
 )
 
-type SimpleChaincode struct {
-}
+// User defined chain code struct
+type SimpleChaincode struct{}
 
-func main() {
-   err := shim.Start(new(SimpleChaincode))
-   if err != nil {
-      fmt.Printf("Error starting Simple chaincode: %s", err)
-   }
-}
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-   return shim.Success(nil)
+	return shim.Success(nil)
 }
-
 
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-   function, args := stub.GetFunctionAndParameters()
-   fmt.Println("invoke is running " + function)
-   if function == "test1" {//自定义函数名称
-      return t.test1(stub, args)//定义调用的函数
-   }
-   return shim.Error("Received unknown function invocation")
+	function, args := stub.GetFunctionAndParameters()
+	fmt.Println("invoke is running " + function)
+
+	if function == "test1" {
+		return t.test1(stub, args)
+	} else {
+		return shim.Error("Received unknown function invocation")
+	}
 }
-func (t *SimpleChaincode) test1(stub shim.ChaincodeStubInterface, args []string) pb.Response{
-   return shim.Success([]byte("Called test1"))
+
+func (t *SimpleChaincode) test1(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success([]byte("Called test1"))
+}
+
+// End User defined chain code struct
+
+// User defined smart contract
+type SmartContract struct{}
+
+func (t *SmartContract) insert_record(stub shim.ChaincodeStubInterface, args []string) string {
+	// TODO: Implement insert_record function
+	return "key of inserted record"
+}
+
+func (t *SmartContract) update_record(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// TODO: Implement update_record
+	return shim.Success([]byte("Call update"))
+}
+
+func (t *SmartContract) delete_record(stud shim.ChaincodeStubInterface, args []string) pb.Response {
+	// TODO: Implement delete_record
+	return shim.Success([]byte("Call delete"))
+}
+
+// End user defined smart contrct
+
+// Data type
+type Transaction struct {
+	DateTime string `json:"date_time"`
+	Type     string `json:"sum"`
+}
+
+func (t *Transaction) generate_key() string {
+	// TODO: implement key generator function, return value must be unique for every different instance
+	// and be constant to same Transaction object
+	return "dumb_key"
+}
+
+// End date type
+
+func main() {
+	err := shim.Start(new(SimpleChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
 }
